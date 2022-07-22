c2f32516d5a24f20ddbb2983f9df6026a20f501c
celitos <93286134+celitos@users.noreply.github.com>
2022-07-21 22:32:18 -0300

    Create command_001.c

---
+++ command_001.c
@@ -0,0 +1,22 @@
+public class CommandManager : MonoBehaviour
+{
+   public interface ICommand
+   {
+       void Execute();
+   }
+  
+   public static CommandManager Instance { get; private set; }
+
+   private Stack<ICommand> m_CommandsBuffer = new Stack<ICommand>();
+
+   private void Awake()
+   {
+       Instance = this;
+   }
+
+   public void AddCommand(ICommand command)
+   {
+       command.Execute();
+       m_CommandsBuffer.Push(command);
+   }
+}
